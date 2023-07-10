5215445441f1063d6a745a059275f63733abaecc
Kenyansa <kenyansajustine@gmail.com>
2023-07-10 17:00:38 +0300

    add needed control flow statements

--- script.js
+++ script.js
@@ -8,5 +8,22 @@ function getAkanName(){
     //defining Akan names in an array for males and females
     let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     let femaleNames = ["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua", "Ama"];
-    
+
+    //show Akan name based on the day of the week
+    if(!isNaN(dayOfWeek) && dayOfWeek >=0 && dayOfWeek <=6){
+        let akanName = null;
+        if (gender === "male"){
+            akanName = maleNames[dayOfWeek];
+        }else if(gender === "female"){
+            akanName = femaleNames[dayOfWeek];
+        }
+
+        if (akanName){
+            alert("Your Akan Name is: "+ akanName);
+        } else {
+            alert("Invalid gender.");
+        }
+    }else{
+        alert("Invalid date of birth");
+    }
 }
